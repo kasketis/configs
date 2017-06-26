cabf8dd995cdcf831f572eab85a2ea0ac4229d04
kasketis <kasketis@gmail.com>
2017-06-26 18:14:25 +0300

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,19 @@
+syntax on
+
+set undodir=~/.vim/undodir
+set undofile
+set undolevels=1000
+set undoreload=10000
+
+set number
+set ignorecase
+set smartcase
+set ruler
+set cursorline
+set encoding=utf-8
+set showmatch
+
+set expandtab
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#prereqs
+#zsh
+#oh-my-zsh
+
+git pull
+
+cd "$(dirname "$0")"
+
+for file in $(ls -a|grep "^\.[a-z]"|grep -v "^\.git$")
+do
+read -p "Create link for file: $file? [y/n] "
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      yes=true
+    else 
+      yes=false
+    fi
+  if [ "$yes" = true ]; then
+    echo "Linking file $file"
+    ln -v -sf `pwd`/$file ~
+  fi
+done
