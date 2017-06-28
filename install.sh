8dc2c86514850a03cd10fc8932421e7245c557c6
kasketis <kasketis@gmail.com>
2017-06-28 10:29:52 +0300

    Update: install.sh

--- install.sh
+++ install.sh
@@ -4,19 +4,21 @@
 #zsh
 #oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh)
 
-echo "Installation started"
+echo "Script started"
+
+read -p "Install oh-my-zsh? [y/n] "
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    echo "oh-my-zsh installation finished."
+fi
 
 for file in $(ls -a|grep "^\.[a-z]"|grep -v "^\.git$")
 do
 read -p "Create link for file: $file? [y/n] "
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-      yes=true
-    else 
-      yes=false
+        echo "Linking file $file: " 
+        cp ~/$file ~/$file".old" 2>>/dev/null #backup old 
+        ln -v -sf `pwd`/$file ~
+        echo "" 
     fi
-  if [ "$yes" = true ]; then
-    echo "Linking file $file: " 
-    ln -v -sf `pwd`/$file ~
-    echo ""
-  fi
 done
